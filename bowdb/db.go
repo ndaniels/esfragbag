ea1e7ba8ed488178b92e8d20d37da4fcac0768ec
Barış Ekim <ekimb@csail.mit.edu>
2015-07-29 15:03:33 -0400

    Revert "path"
    This reverts commit 7630499b1d9c8ddf96c7c44b9bda5d34f557aa03.

--- bowdb/db.go
+++ bowdb/db.go
@@ -21,7 +21,7 @@ import (
 
 const (
 	fileBowDB   = "bow.db"
-	fileFragLib = "/Users/barisekim/go/src/github.com/ndaniels/flib/flibs-structure/400-11.json"
+	fileFragLib = "frag-lib.json"
 )
 
 // DB represents a BOW database. It is always connected to a particular
